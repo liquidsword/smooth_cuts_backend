62c29ea1ec309a202bdd9c1df57d18f5e3cd3ba6
liquidsword <audenrobertson@gmail.com>
2020-02-28 20:32:39 -0500

    Updated cors
    namespaced my sessions controller. Add a Userserializer and updated cors so I can use the proper ports for my backend and frontend

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,10 +1,10 @@
-class SessionsController < ApplicationController
+class Api::V1::SessionsController < ApplicationController
   def create
       @user = User.find_by(username: params[:session][:username])
 
       if @user && @user.authenticate(params[:session][:password])
         session[:user_id] = @user.id
-        render json: @user
+        render json: UserSerializer.new(@user)
       else
         render json: {
           error: "Invalid Credentials"
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
 
   def get_current_user
     if logged_in?
-    render json: current_user
+    render json: UserSerializer.new(current_user)
   else
     render json: {
       error: "No one logged in"
